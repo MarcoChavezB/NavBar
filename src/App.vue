48d33b6b8f44a7d1b8504337b35dba38a2797d7d
Marco <marco1102004@gmail>
2023-06-27 17:59:23 -0600

    empezando el proyecto

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="app">
+    <div class="nav">
+      <h1>hola mundo</h1>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .app{
+    display: grid;
+    grid-template-columns: 15% 90%;
+    grid-template-rows: 2fr 100px;
+    grid-column-gap: 20px;
+    grid-row-gap: 20px;
+    justify-items: stretch;
+    align-items: stretch;
+  }
+  .nav{
+    position: absolute;
+    background-color: azure;
+    height: 100vh;
+  }
+</style>
