784cff5846d469cdea415f4b5e008b96810284a8
anthony <tony20402014@gmail.com>
2023-06-27 19:24:52 -0600

    agregando un componente para views

---
+++ src/views/componente1.vue
@@ -0,0 +1,41 @@
+<template>
+<div class="pantallacom">
+    <div class="divi">
+        <div class="cont"> 
+         CONTENIDO 1 
+        </div>
+        <div class="cont">
+         CONTENIDO 2
+        </div>
+    </div>
+    <div> 
+    </div>
+</div>
+</template>
+
+<style scoped>
+
+.pantallacom{
+    width: 100vh;
+    height: 100vh;
+    display: grid;
+    grid-template-columns: 50% 50%;
+}
+
+.divi{
+    grid-column-start: 2;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+}
+
+.cont{
+    width: 20vh;
+    height: 15vh;
+    box-shadow: black;
+    border-radius: 10px;
+    margin-bottom: 1em;
+    border-color: beige;
+}
+
+</style>
